6b109081464b4cebace9ab1c67cddfb2ea9521f9
m4rbury <marbury@protonmail.ch>
2016-02-23 17:43:10 -0800

    Added files via upload

--- o.py
+++ o.py
@@ -7,7 +7,7 @@ x=0
 saveFile = open('test-replace.csv', 'w')
 
 for eachStyle in style:
-	saveLine = eachStyle[1:].strip("\'") + '_' + color[x][1:].strip("\'") + '_' + size[x][1:].strip("\'") + '\n'
+	saveLine = eachStyle[1:].strip("\'") + '_' + color[x][1:].strip("\'") + '_' + size[x][1:].strip("\'") + ',' + quantity[x][1:].strip("\'") + '\n'
 	x = x + 1
 	print(saveLine)
 	saveFile.write(saveLine)
